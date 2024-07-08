4549b71ce90f4293b4c5981a089bcaa3dc08e13c
speedcuberayush <msdakm3334@gmail.com>
2024-07-08 12:11:13 +0530

    session monday

---
+++ DSA/DP/dp6/regexconversion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isMatch(string s, string p)
+    {
+        vector<bool> dp(p.size() + 1, false);
+        dp[0] = true;
+        for (int j = 2; j <= p.size(); ++j)
+            if (p[j - 1] == '*')
+                dp[j] = dp[j - 2];
+
+        for (int i = 1; i <= s.size(); ++i)
+        {
+            bool previous = dp[0];
+            dp[0] = false;
+            
+            for (int j = 1; j <= p.size(); ++j)
+            {
+                bool curr = dp[j];
+                if (p[j - 1] != '*')
+                    dp[j] = (p[j - 1] == '.' || p[j - 1] == s[i - 1]) && previous;
+                else
+                    dp[j] = dp[j - 2] || (((p[j - 2] == '.') || (p[j - 2] == s[i - 1])) && curr);
+                previous = curr;
+            }
+        }
+        return dp[p.size()];
+    }
+};
